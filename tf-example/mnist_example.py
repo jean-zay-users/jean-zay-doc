d6228972c0b31169f5464d7bdd50f1eafad4d3a4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-27 16:11:00 +0100

    using random data in the example because network is unreachable

--- tf-example/mnist_example.py
+++ tf-example/mnist_example.py
@@ -14,9 +14,12 @@ outputs = layers.Dense(10)(x)
 model = keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
 
 # training and inference
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-x_train = x_train.reshape(60000, 784).astype('float32') / 255
-x_test = x_test.reshape(10000, 784).astype('float32') / 255
+# network is not reachable, so we use random data
+x_train = tf.random.normal((60000, 784), dtype='float32')
+x_test = tf.random.normal((10000, 784), dtype='float32')
+y_train = tf.random.uniform((60000,), minval=0, maxval=10, dtype='int32')
+y_test = tf.random.uniform((10000,), minval=0, maxval=10, dtype='int32')
+
 
 model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               optimizer=keras.optimizers.RMSprop(),
