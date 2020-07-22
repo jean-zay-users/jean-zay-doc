08162259122dd2b8412844688d0b6f36e845b9db
olivierD <olivier.dehaene@gmail.com>
2020-07-22 11:51:41 +0200

    Changed default values

--- examples/pytorch/distributed_stl10/pytorch_distributed_stl10_example.sh
+++ examples/pytorch/distributed_stl10/pytorch_distributed_stl10_example.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=03:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=00:10:00              # maximum execution time (HH:MM:SS)
 #SBATCH --output=pytorch_stl10%j.out # output file name
 #SBATCH --error=pytorch_stl10%j.err  # error file name
 
--- examples/pytorch/distributed_stl10/stl10_example.py
+++ examples/pytorch/distributed_stl10/stl10_example.py
@@ -60,7 +60,7 @@ def accuracy(output: torch.Tensor,
 def reduce_tensor(tensor: torch.Tensor, world_size: int):
     """Reduce tensor across all nodes."""
     rt = tensor.clone()
-    dist.all_reduce(rt, op=dist.reduce_op.SUM)
+    dist.all_reduce(rt, op=dist.ReduceOp.SUM)
     rt /= world_size
     return rt
 
@@ -340,7 +340,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch STL10 Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=.1, metavar='LR',
                         help='learning rate (default: .1)')
