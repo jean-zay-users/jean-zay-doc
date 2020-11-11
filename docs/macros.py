674a1244572dd99f8b76a3e4b2d79afaba648afd
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-11-11 12:02:51 +0100

    Add mkdocs macro to insert code snippet from code files into documentation

---
+++ docs/macros.py
@@ -0,0 +1,42 @@
+## Macros file to be used with mkdocs_macros_plugin
+## This snippet is taken from this issue 
+## https://github.com/mkdocs/mkdocs/issues/692
+
+import html
+import os
+
+def declare_variables(variables, macro):
+    @macro
+    def code_from_file(fn: str, flavor: str = ""):
+        """
+        Load code from a file and save as a preformatted code block.
+        If a flavor is specified, it's passed in as a hint for syntax highlighters.
+
+        Example usage in markdown:
+
+            {{code_from_file("code/myfile.py", "python")}}
+
+        """
+        docs_dir = variables.get("docs_dir", "docs")
+        fn = os.path.abspath(os.path.join(docs_dir, fn))
+        if not os.path.exists(fn):
+            return f"""<b>File not found: {fn}</b>"""
+        with open(fn, "r") as f:
+            source = f.read()
+            return f"```{flavor}\n{source}```" 
+
+    @macro
+    def external_markdown(fn: str):
+        """
+        Load markdown from files external to the mkdocs root path.
+        Example usage in markdown:
+
+            {{external_markdown("../../README.md")}}
+
+        """
+        docs_dir = variables.get("docs_dir", "docs")
+        fn = os.path.abspath(os.path.join(docs_dir, fn))
+        if not os.path.exists(fn):
+            return f"""<b>File not found: {fn}</b>"""
+        with open(fn, "r") as f:
+            return f.read()
