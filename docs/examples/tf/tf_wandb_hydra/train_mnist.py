48599053df1c0936c1b679d4cc0fb3f3a6d29e0e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-01-07 14:28:31 +0100

    added the functions in the script to keep the imports of tensorflow and wandb lazy

--- docs/examples/tf/tf_wandb_hydra/train_mnist.py
+++ docs/examples/tf/tf_wandb_hydra/train_mnist.py
@@ -10,30 +10,6 @@ def train_dense_model_main(cfg):
     return train_dense_model(cfg)
 
 
-def my_model(input_shape=784, output_num=10, activation='relu', hidden_size=64):
-    inputs = keras.Input(shape=input_shape)
-    x = layers.Dense(hidden_size, activation=activation)(inputs)
-    x = layers.Dense(hidden_size, activation=activation)(x)
-    outputs = layers.Dense(output_num)(x)
-    return keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
-
-def model_compile(model, loss='xent', optimizer='rmsprop'):
-    if loss == 'xent':
-        loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    model.compile(loss=loss,
-                  optimizer=optimizer,
-                  metrics=['accuracy'])
-
-def data(n_train=60_000, n_test=10_000, n_features=784, n_classes=10):
-    # training and inference
-    # network is not reachable, so we use random data
-    x_train = tf.random.normal((n_train, n_features), dtype='float32')
-    x_test = tf.random.normal((n_test, n_features), dtype='float32')
-    y_train = tf.random.uniform((n_train,), minval=0, maxval=n_classes, dtype='int32')
-    y_test = tf.random.uniform((n_test,), minval=0, maxval=n_classes, dtype='int32')
-    return x_train, x_test, y_train, y_test
-
-
 def train_dense_model(cfg):
     # limit imports oustide the call to the function, in order to launch quickly
     # when using dask
@@ -42,6 +18,31 @@ def train_dense_model(cfg):
     from tensorflow.keras import layers
     import wandb
     from wandb.keras import WandbCallback
+
+
+    def my_model(input_shape=784, output_num=10, activation='relu', hidden_size=64):
+        inputs = keras.Input(shape=input_shape)
+        x = layers.Dense(hidden_size, activation=activation)(inputs)
+        x = layers.Dense(hidden_size, activation=activation)(x)
+        outputs = layers.Dense(output_num)(x)
+        return keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
+
+    def model_compile(model, loss='xent', optimizer='rmsprop'):
+        if loss == 'xent':
+            loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        model.compile(loss=loss,
+                    optimizer=optimizer,
+                    metrics=['accuracy'])
+
+    def data(n_train=60_000, n_test=10_000, n_features=784, n_classes=10):
+        # training and inference
+        # network is not reachable, so we use random data
+        x_train = tf.random.normal((n_train, n_features), dtype='float32')
+        x_test = tf.random.normal((n_test, n_features), dtype='float32')
+        y_train = tf.random.uniform((n_train,), minval=0, maxval=n_classes, dtype='int32')
+        y_test = tf.random.uniform((n_test,), minval=0, maxval=n_classes, dtype='int32')
+        return x_train, x_test, y_train, y_test
+
 
     # wandb setup
     Path(cfg.wandb.dir).mkdir(exist_ok=True, parents=True)
